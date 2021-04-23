05bfac7c5c59e9c0da7ed9ecfc9f21f702786257
Yassmine Romdhane <yassmineromdhane1996@gmail.com>
2021-04-23 17:31:09 +0100

    refactoring code

--- portfolio/src/app/portfolio/mockData.ts
+++ portfolio/src/app/portfolio/mockData.ts
@@ -1,105 +1,114 @@
 export const BUTTONS = [{
-    skill: "All Projects",
-  },
-  {
-    skill: "React",
-  }, {
-    skill: "Angular",
-  },
-  {
-    skill: "mongoDB",
-  }, {
-  skill: "vue",
-},{
-    skill: "MySql",
-  }]
- export const PROJECTS= [{
-    image: "../assets/shop.png",
-    skills: ["#React", "#MySql", "#NodeJs"],
-    title: "MVP Project:Shoppinettes",
-    description: "This project is a shopping site where I used React, NodJs and mySql. i implemented all CRUD operations",
-    demo: "",
-    code: "https://github.com/YassmineRomdhane/rbktnX-mvp-starter"
-  },
-  {
-    image: "../assets/Gary.png",
-    skills: ["#Angular", "#mongoDB", "#NodeJs"],
-    title: "Thesis Project: GaryWork",
-    description: "This project is a platform meant to provide users with different services related to house needs",
-    demo: "",          
-    code: "https://github.com/The-Garys"
-  },
-  {
-    image: "../assets/amazon.png",
-    skills: ["#React", "#NodeJs"],
-    title: "FrontEndCapstone",
-    description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
-    demo: "demo",
-    code: "https://github.com/Explosion2k21"
-  },
+  skill: "All Projects",
+},
+{
+  skill: "React",
+}, {
+  skill: "Angular",
+},
 
-  {
-    image: "https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg",
-    skills: ["#React", "#MySql", "#NodeJs"],
-    title: "MVP Project:Shoppinettes",
-    description: "This project is a shopping site where I used React, NodJs and mySql. i implemented all CRUD operations",
-    demo: "",
-    code: "https://github.com/YassmineRomdhane/rbktnX-mvp-starter"
-  },
-  {
-    image: "https://www.planetware.com/wpimages/2019/10/switzerland-in-pictures-most-beautiful-places-matterhorn.jpg",
-    skills: ["#Angular", "#mongoDB", "#NodeJs"],
-    title: "Thesis Project: GaryWork",
-    description: "This project is a platform meant to provide users with different services related to house needs",
-    demo: "",          
-    code: "https://github.com/The-Garys"
-  },
-  {
-    image: "https://miefly.com/wp-content/uploads/2020/11/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-    skills: ["#React", "#NodeJs"],
-    title: "FrontEndCapstone",
-    description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
-    demo: "demo",
-    code: "https://github.com/Explosion2k21"
-  },
-  {
-    image: "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    skills: ["#React", "#MySql", "#NodeJs"],
-    title: "MVP Project:Shoppinettes",
-    description: "This project is a shopping site where I used React, NodJs and mySql. i implemented all CRUD operations",
-    demo: "",
-    code: "https://github.com/YassmineRomdhane/rbktnX-mvp-starter"
-  },
-  {
-    image: "https://www.planetware.com/wpimages/2019/10/switzerland-in-pictures-most-beautiful-places-matterhorn.jpg",
-    skills: ["#Angular", "#mongoDB", "#NodeJs"],
-    title: "Thesis Project: GaryWork",
-    description: "This project is a platform meant to provide users with different services related to house needs",
-    demo: "",          
-    code: "https://github.com/The-Garys"
-  },
-  {
-    image: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    skills: ["#React", "#NodeJs"],
-    title: "FrontEndCapstone",
-    description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
-    demo: "demo",
-    code: "https://github.com/Explosion2k21"
-  },
-  ]
+{
+  skill: "jQuery",
+},
+// {
+//   skill: "CSS",
+// },
+// {
+//   skill: "HTML",
+// },
+{
+  skill: "mongoDB",
+},  {
+  skill: "MySql",
+}]
+export const PROJECTS = [{
+  image: "../assets/shop.png",
+  skills: ["#React", "#MySql", "#NodeJs"],
+  title: "MVP Project:Shoppinettes",
+  description: "This project is a shopping site where I used React, NodJs and mySql. i implemented all CRUD operations",
+  demo: "",
+  code: "https://github.com/YassmineRomdhane/rbktnX-mvp-starter"
+},
+{
+  image: "../assets/Gary.png",
+  skills: ["#Angular", "#mongoDB", "#NodeJs"],
+  title: "Thesis Project: GaryWork",
+  description: "This project is a platform meant to provide users with different services related to house needs",
+  demo: "",
+  code: "https://github.com/The-Garys"
+},
+{
+  image: "../assets/movies.png",
+  skills: ["#jQuery"],
+  title: "M&Y Movies",
+  description: "For this project I used CSS, Html and jQuery",
+  demo: "",
+  code: "https://github.com/YassmineRomdhane/Duo-project-Movies-"
+},
+{
+  image: "../assets/amazon.png",
+  skills: ["#React", "#NodeJs"],
+  title: "FrontEndCapstone",
+  description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
+  demo: "demo",
+  code: "https://github.com/Explosion2k21"
+},
 
-  export const BlOGS=[
-    {
-      title:"Why do you always need to learn new Technologies?",
-      image:"https://cdn-images-1.medium.com/max/800/1*wwgaOnLdrGHkjUU3g1SDhg.jpeg",
-      body:" we are living in the generation of technologies",
-      link:""
-   } ,
-   {
-     title:"A world without developers!",
-     image:"https://miro.medium.com/max/1145/1*pKpqN0Mw45eSQQKypC9Vsw.png",
-     body:"Programming is very important for our future, I really can not imagine a world without developers , since they are making our life easier , also we can not improve our life without internet, its like a magical think which can change people’s lives",
-     link:"https://medium.com/me/stories/public"
-     }
-     
-  ]
+
+{
+  image: "../assets/flowers.png",
+  skills: ["#CSS", "#HTML", "#jQuery"],
+  title: "FlowerShop",
+  description: "This project is my first project, I used pure CSS and HTML",
+  demo: "",
+  code: "https://github.com/YassmineRomdhane/-solo-project-"
+},
+{
+  image: "https://miefly.com/wp-content/uploads/2020/11/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+  skills: ["#React", "#NodeJs"],
+  title: "FrontEndCapstone",
+  description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
+  demo: "demo",
+  code: "https://github.com/Explosion2k21"
+},
+{
+  image: "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  skills: ["#React", "#MySql", "#NodeJs"],
+  title: "MVP Project:Shoppinettes",
+  description: "This project is a shopping site where I used React, NodJs and mySql. i implemented all CRUD operations",
+  demo: "",
+  code: "https://github.com/YassmineRomdhane/rbktnX-mvp-starter"
+},
+{
+  image: "https://www.planetware.com/wpimages/2019/10/switzerland-in-pictures-most-beautiful-places-matterhorn.jpg",
+  skills: ["#Angular", "#mongoDB", "#NodeJs"],
+  title: "Thesis Project: GaryWork",
+  description: "This project is a platform meant to provide users with different services related to house needs",
+  demo: "",
+  code: "https://github.com/The-Garys"
+},
+{
+  image: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  skills: ["#React", "#NodeJs"],
+  title: "FrontEndCapstone",
+  description: "This projects was about mimicking a single page details of Amazon and I interacted with a provided API",
+  demo: "demo",
+  code: "https://github.com/Explosion2k21"
+},
+]
+
+export const BlOGS = [
+  {
+    title: "Why do you always need to learn new Technologies?",
+    image: "https://cdn-images-1.medium.com/max/800/1*wwgaOnLdrGHkjUU3g1SDhg.jpeg",
+    body: " we are living in the generation of technologies",
+    link: ""
+  },
+  {
+    title: "A world without developers!",
+    image: "https://miro.medium.com/max/1145/1*pKpqN0Mw45eSQQKypC9Vsw.png",
+    body: "Programming is very important for our future, I really can not imagine a world without developers , since they are making our life easier , also we can not improve our life without internet, its like a magical think which can change people’s lives",
+    link: "https://medium.com/me/stories/public"
+  }
+
+]
--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -22,53 +22,22 @@ export class PortfolioComponent implements OnInit {
     this.projects=PROJECTS
   }
   filterByTags(val) {
-    console.log("ssss",val)
+    console.log("vaaaal",val)
     this.ngOnInit()
     var newArr = []
-    const hashtag="#"+val
+    const tag="#"+val
+    console.log("hashtag",tag)
     
     if (val !== 'All Projects') {
     this.projects.map((e) => {
     // val=val.toUpperCase()
-      if (e.skills.includes(hashtag)) {
+      if (e.skills.includes(tag)) {
       newArr.push(e)
       }
      })
     this.projects = newArr
   }  
-   
- 
-  }
-  // filterByTags(val) {
-  //   var newArr = []
-  //   let reg = new RegExp(val,'g');
-  // let reg = new RegExp(val,'g');
-  //   this.projects.map((e) => {
-  //     val=val.toUpperCase()
-  //     var skill=e.skills.toUpperCase()
-  //     if (skill.matchAll(reg)) {
+    }
 
-  //       newArr.push(e)
-      
-  //     }
-  //   })
-  //   this.projects = newArr
-  //   this.ngOnInit()
-
-  // }
-
-//   filterByTags(val) {
-//     console.log("ssss",val)
-//     var newArr = []
-//     this.projects.filter((event) => {
-
-//       if(event.skills.includes(val)) {
-//         newArr.push(event)
-//         console.log("gggg",newArr)
-//       }
-//     });
-//     this.projects = newArr
-//     this.ngOnInit()
-// } 
 }
 
